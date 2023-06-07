447ef3327687686082c34dd32a0c83eecf908ab8
Aziza H <mess.aziza@gmail.com>
2023-06-07 10:14:33 +0200

    l'ajout de la librairie react table

--- src/pages/ListEmployee.jsx
+++ src/pages/ListEmployee.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useMemo} from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import {MaterialReactTable} from "material-react-table";
 
 import {employeeTab } from "../redux/reducers/employeeReducer";
 
@@ -8,11 +9,117 @@ import {employeeTab } from "../redux/reducers/employeeReducer";
 export default function ListEmployee() {
   const employeesList = useSelector(employeeTab)
   console.log(employeesList)
+  const columns = useMemo(
+		() => [
+			{
+				header: "First Name",
+				accessorKey: "firstName",
+				size: 100,
+			},
+			{
+				header: "Last Name",
+				accessorKey: "lastName",
+				size: 100,
+			},
+			{
+				header: "Start Date",
+				accessorKey: "startDate",
+				size: 100,
+			},
+			{
+				header: "Department",
+				accessorKey: "department",
+				size: 100,
+			},
+			{
+				header: "Date of Birth",
+				accessorKey: "dateBirth",
+				size: 100,
+			},
+			{
+				header: "Street",
+				accessorKey: "street",
+				size: 100,
+			},
+			{
+				header: "City",
+				accessorKey: "city",
+				size: 100,
+			},
+			{
+				header: "State",
+				accessorKey: "state",
+				size: 100,
+			},
+			{
+				header: "Zip Code",
+				accessorKey: "zipCode",
+				size: 100,
+			},
+		],
+		[]
+	);
+
   
   return (
     <div id="employee-div" className="container">
       <h1>Current Employees</h1>
-      <table id="employee-table" className="display"></table>
+      {employeesList ? (
+				<MaterialReactTable
+					columns={columns}
+					data={employeesList}
+         
+          initialState={
+            {
+              pagination: {
+                pageSize: 10,
+                pageIndex: 0
+              },
+              density: 'compact',
+              showGlobalFilter: true 
+            }
+            
+          }
+         // enableRowNumbers //activate row number
+         // rowNumberMode="original"
+          positionGlobalFilter="right" //barsearch right
+          enableToolbarInternalActions={false}//desable global filter
+          muiTableBodyCellProps={{
+            sx: {
+              p: '2px 16px', //padding cells
+              border: '1px solid #f2f2f2'
+            },
+            align:'center'
+          }}
+          muiTableBodyProps={{
+            sx: () => ({
+              '& tr:nth-of-type(odd)': {
+                backgroundColor: '#f2f2f2' //cells color 
+              },
+              
+            }),
+            
+          }} 
+          
+          muiTablePaperProps={{
+            sx: {
+              maxWidth: '99%',// width of table
+              m: 'auto',
+             
+            },
+            elevation: 0 //no box shodow
+            
+          }}
+          muiTableHeadCellProps={{
+            align: 'center',
+          }}
+          
+          
+				
+				/>
+			) : (
+				<span>No data to display</span>
+			)}
       <Link to="/">Home</Link>
     </div>
 
